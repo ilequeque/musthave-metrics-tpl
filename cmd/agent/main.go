3391c9005d3695259f138d098b392bd57f866179
ilequeque <ilyas.shakhabdinov@gmail.com>
2025-11-07 18:12:43 +0500

    iter4 implementation

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"net/http"
@@ -8,15 +9,18 @@ import (
 	"time"
 )
 
-const (
-	serverAddr     = "http://localhost:8080"
-	pollInterval   = 2 * time.Second
-	reportInterval = 10 * time.Second
-)
-
 func main() {
+	// флаги
+	addr := flag.String("a", "http://localhost:8080", "server address")
+	reportInt := flag.Int("r", 10, "report interval in seconds")
+	pollInt := flag.Int("p", 2, "poll interval in seconds")
+	flag.Parse()
+
 	client := &http.Client{Timeout: 5 * time.Second}
 	rand.Seed(time.Now().UnixNano())
+
+	pollInterval := time.Duration(*pollInt) * time.Second
+	reportInterval := time.Duration(*reportInt) * time.Second
 
 	var pollCount int64
 	metrics := make(map[string]float64)
@@ -38,10 +42,10 @@ func main() {
 
 		case <-tReport.C:
 			for name, value := range metrics {
-				sendMetric(client, "gauge", name, value)
+				sendMetric(client, *addr, "gauge", name, value)
 			}
-			sendMetric(client, "counter", "PollCount", pollCount)
-			fmt.Println("metrics sent to server")
+			sendMetric(client, *addr, "counter", "PollCount", pollCount)
+			fmt.Println("metrics sent to", *addr)
 		}
 	}
 }
@@ -52,37 +56,37 @@ func collectMetrics() map[string]float64 {
 
 	return map[string]float64{
 		"Alloc":         float64(ms.Alloc),
-		"BuckHashSys":   float64(ms.BuckHashSys),
+		"TotalAlloc":    float64(ms.TotalAlloc),
+		"Sys":           float64(ms.Sys),
+		"Lookups":       float64(ms.Lookups),
+		"Mallocs":       float64(ms.Mallocs),
 		"Frees":         float64(ms.Frees),
-		"GCCPUFraction": ms.GCCPUFraction,
-		"GCSys":         float64(ms.GCSys),
 		"HeapAlloc":     float64(ms.HeapAlloc),
+		"HeapSys":       float64(ms.HeapSys),
 		"HeapIdle":      float64(ms.HeapIdle),
 		"HeapInuse":     float64(ms.HeapInuse),
-		"HeapObjects":   float64(ms.HeapObjects),
 		"HeapReleased":  float64(ms.HeapReleased),
-		"HeapSys":       float64(ms.HeapSys),
-		"LastGC":        float64(ms.LastGC),
-		"Lookups":       float64(ms.Lookups),
-		"MCacheInuse":   float64(ms.MCacheInuse),
-		"MCacheSys":     float64(ms.MCacheSys),
-		"MSpanInuse":    float64(ms.MSpanInuse),
-		"MSpanSys":      float64(ms.MSpanSys),
-		"Mallocs":       float64(ms.Mallocs),
-		"NextGC":        float64(ms.NextGC),
-		"NumForcedGC":   float64(ms.NumForcedGC),
-		"NumGC":         float64(ms.NumGC),
-		"OtherSys":      float64(ms.OtherSys),
-		"PauseTotalNs":  float64(ms.PauseTotalNs),
+		"HeapObjects":   float64(ms.HeapObjects),
 		"StackInuse":    float64(ms.StackInuse),
 		"StackSys":      float64(ms.StackSys),
-		"Sys":           float64(ms.Sys),
-		"TotalAlloc":    float64(ms.TotalAlloc),
+		"MSpanInuse":    float64(ms.MSpanInuse),
+		"MSpanSys":      float64(ms.MSpanSys),
+		"MCacheInuse":   float64(ms.MCacheInuse),
+		"MCacheSys":     float64(ms.MCacheSys),
+		"BuckHashSys":   float64(ms.BuckHashSys),
+		"GCSys":         float64(ms.GCSys),
+		"OtherSys":      float64(ms.OtherSys),
+		"NextGC":        float64(ms.NextGC),
+		"LastGC":        float64(ms.LastGC),
+		"PauseTotalNs":  float64(ms.PauseTotalNs),
+		"NumGC":         float64(ms.NumGC),
+		"NumForcedGC":   float64(ms.NumForcedGC),
+		"GCCPUFraction": ms.GCCPUFraction,
 	}
 }
 
-func sendMetric(client *http.Client, metricType, name string, value interface{}) {
-	url := fmt.Sprintf("%s/update/%s/%s/%v", serverAddr, metricType, name, value)
+func sendMetric(client *http.Client, addr, metricType, name string, value interface{}) {
+	url := fmt.Sprintf("%s/update/%s/%s/%v", addr, metricType, name, value)
 	req, err := http.NewRequest(http.MethodPost, url, nil)
 	if err != nil {
 		fmt.Println("request create error:", err)
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,16 +13,18 @@ import (
 )
 
 func main() {
+	addr := flag.String("a", "localhost:8080", "address for HTTP server")
+	flag.Parse()
+
 	storage := repository.NewMemStorage()
 	service := service.NewMetricService(storage)
 	handler := handler.NewMetricHandler(service, storage)
 
 	r := chi.NewRouter()
-
 	r.Post("/update/{type}/{name}/{value}", handler.Update)
 	r.Get("/value/{type}/{name}", handler.GetValue)
 	r.Get("/", handler.GetAllMetrics)
 
-	fmt.Println("Server running on :8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	fmt.Printf("Server running on %s\n", *addr)
+	log.Fatal(http.ListenAndServe(*addr, r))
 }
