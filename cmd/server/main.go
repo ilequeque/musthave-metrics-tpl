855b214196a0c821811f8a27b290b7f222e7a678
ilequeque <ilyas.shakhabdinov@gmail.com>
2025-11-22 15:14:58 +0500

    feat(iter6): add request/response logging middleware with logrus

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,24 +7,32 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/ilequeque/musthave-metrics-tpl/internal/config"
 	"github.com/ilequeque/musthave-metrics-tpl/internal/handler"
+	"github.com/ilequeque/musthave-metrics-tpl/internal/middleware"
 	"github.com/ilequeque/musthave-metrics-tpl/internal/repository"
 	"github.com/ilequeque/musthave-metrics-tpl/internal/service"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	cfg := config.ParseServerFlags()
+
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+	logrus.SetLevel(logrus.InfoLevel)
 
 	st := repository.NewMemStorage()
 	svc := service.NewMetricService(st)
 	h := handler.NewMetricHandler(svc, st)
 
 	r := chi.NewRouter()
+
+	r.Use(middleware.LoggerMiddleware)
+
 	r.Post("/update/{type}/{name}/{value}", h.Update)
 	r.Get("/value/{type}/{name}", h.GetValue)
 	r.Get("/", h.GetAllMetrics)
 
-	log.Printf("server running on %s", cfg.Addr)
-	if err := http.ListenAndServe(cfg.Addr, r); err != nil {
-		log.Fatalf("server error: %v", err)
-	}
+	logrus.Infof("server running on %s", cfg.Addr)
+	log.Fatal(http.ListenAndServe(cfg.Addr, r))
 }
